30bf36025bcdb2dd0b55e41f2c3270e0f132b725
Srivatsa Rv <srivatsa.workspace@gmail.com>
2019-09-12 05:28:31 +0530

    Learnt to print multiple quotes using random function variations

--- get-quote.py
+++ get-quote.py
@@ -5,9 +5,11 @@ def primary():
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
-  last = 13
+  last = 18
   rnd = random.randint(0,last)
   print(quotes[rnd])
+  print(quotes[rnd+1])
+  print(quotes[rnd+2])
 
 if __name__== "__main__":
   primary()
